da52baa79a2a5c2dce33fd8b52d6801c34623399
gustavo panizzo <gfa@zumbi.com.ar>
2019-03-11 13:32:25 +0800

    configure munin-async on nodes

--- site/site_munin/manifests/node.pp
+++ site/site_munin/manifests/node.pp
@@ -4,6 +4,7 @@
 class site_munin::node {
 
   include munin::node
+  include site_munin::node::async
   include site_munin::node::plugins
 
 }
---
+++ site/site_munin/manifests/node/async.pp
@@ -0,0 +1,36 @@
+# This class manages munin-async
+#
+# @param ssh_keys contains the ssh keys to inject for the munin-async user
+#
+
+class site_munin::node::async (
+  Hash $ssh_keys
+) {
+
+  package {'munin-async':
+    ensure => installed,
+  }
+
+  -> user { 'munin-async':
+    ensure         => present,
+    purge_ssh_keys => true,
+    home           => '/var/lib/munin-async',
+  }
+
+  $ssh_keys.each |$key, $data| {
+    ssh_authorized_key { "${key}-munin-async":
+      ensure  => present,
+      type    => $data['type'],
+      options => $data['options'],
+      key     => $data['key'],
+      user    => 'munin-async',
+      require => User['munin-async'],
+    }
+  }
+
+  service { 'munin-async':
+    ensure => running,
+    enable => true,
+  }
+
+}

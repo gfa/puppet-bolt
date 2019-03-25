edd49eb67d1873d243f6ea1ea372062a3cd85a9b
gustavo panizzo <gfa@zumbi.com.ar>
2019-03-25 12:34:26 +0800

    improve the firewall

--- site/site_firewall/manifests/pre.pp
+++ site/site_firewall/manifests/pre.pp
@@ -6,20 +6,20 @@ class site_firewall::pre {
   Firewall { require => undef, }
 
   # INPUT
-  firewall_multi { '001 Allow INPUT RELATED and ESTABLISHED':
+  firewall_multi { '001 allow input related and established':
     chain    => 'INPUT',
     state    => ['RELATED', 'ESTABLISHED'],
     proto    => 'all',
     action   => 'accept',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall_multi { '002 Allow INPUT lo':
+  firewall_multi { '002 allow input lo':
     chain    => 'INPUT',
     proto    => 'all',
     action   => 'accept',
     iniface  => 'lo',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
   firewallchain { 'FILTERS:filter:IPv4':
@@ -34,67 +34,73 @@ class site_firewall::pre {
     chain    => 'INPUT',
     proto    => 'all',
     jump     => 'FILTERS',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall_multi { '100 Allow INPUT SSH':
+  firewall_multi { '100 allow input ssh':
     chain    => 'INPUT',
     proto    => 'tcp',
     dport    => '22',
     state    => 'NEW',
     action   => 'accept',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall { '101 Allow INPUT ICMP echo':
-    chain  => 'INPUT',
-    state  => 'NEW',
-    proto  => 'icmp',
-    icmp   => 'echo-request',
-    action => 'accept',
+  firewall_multi { '101 allow input icmp':
+    chain    => 'INPUT',
+    proto    => 'icmp',
+    icmp     => [0, 3, 8, 11, 42],
+    action   => 'accept',
+    provider => ['iptables'],
   }
 
-  site_firewall::accept_output { 'DHCP client':
-    proto => 'udp',
-    dport => [67, 68],
+  firewall_multi { '101 allow input icmp echo from subnet':
+    chain    => 'INPUT',
+    proto    => 'ipv6-icmp',
+    icmp     => [130, 131, 132, 143, 151, 152, 153],
+    source   => 'fe80::/10',
+    action   => 'accept',
+    provider => ['ip6tables'],
   }
 
-  #  site_firewall::accept_output { 'DNS/UDP':
-  #  proto => 'udp',
-  #  dport => 53,
-  # }
+  firewall_multi { '102 allow input icmp':
+    chain    => 'INPUT',
+    proto    => 'ipv6-icmp',
+    icmp     => [1, 2, 3, 4, 128, 133, 134, 135, 136, 137, 141, 142, 148, 149],
+    action   => 'accept',
+    provider => ['ip6tables'],
+  }
 
-  # OUTPUT
-  firewall_multi { '001 Allow OUTPUT RELATED and ESTABLISHED':
+  firewall_multi { '001 allow output related and established':
     chain    => 'OUTPUT',
     state    => ['RELATED', 'ESTABLISHED'],
     proto    => 'all',
     action   => 'accept',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall_multi { '002 Allow OUTPUT lo':
+  firewall_multi { '002 allow output lo':
     chain    => 'OUTPUT',
     proto    => 'all',
     action   => 'accept',
     outiface => 'lo',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall_multi { '003 Allow OUTPUT for root':
+  firewall_multi { '003 allow output for root':
     chain    => 'OUTPUT',
     proto    => 'all',
     uid      => 'root',
     action   => 'accept',
-    provider => [ 'iptables', 'ip6tables'],
+    provider => ['iptables', 'ip6tables'],
   }
 
-  firewall { '004 Allow OUTPUT ICMP echo':
-    chain  => 'OUTPUT',
-    state  => 'NEW',
-    proto  => 'icmp',
-    icmp   => 'echo-request',
-    action => 'accept',
+  firewall_multi { '004 allow output icmp echo':
+    chain    => 'OUTPUT',
+    proto    => 'icmp',
+    icmp     => [0, 8],
+    action   => 'accept',
+    provider => ['iptables'],
   }
 
   firewall_multi { '005 Allow OUTPUT dnsmasq':

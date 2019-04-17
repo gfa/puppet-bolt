05e821d339fcc11089d66ffcb27baae058f25a1f
gustavo panizzo <gfa@zumbi.com.ar>
2019-04-17 15:49:18 +0800

    stop using the resolv_conf puppet module

--- site/base/manifests/resolvconf.pp
+++ site/base/manifests/resolvconf.pp
@@ -7,15 +7,10 @@ class base::resolvconf {
     ensure => present,
   }
 
-  class { 'resolv_conf':
-    nameservers => ['127.0.0.1'],
-    searchpath  => [''],
-    require     => Package['dnsmasq'],
-  }
-
   file { '/etc/resolv.conf':
     ensure => link,
     target => '/etc/resolvconf/run/resolv.conf',
+    require     => Package['dnsmasq', 'resolvconf'],
   }
 
 }

02480f090cefa1dc8a57048912a8c5ff540d94de
gustavo panizzo <gfa@zumbi.com.ar>
2019-05-18 16:22:41 +0800

    add a prefix to iptables' log messages

--- site/site_firewall/manifests/pre.pp
+++ site/site_firewall/manifests/pre.pp
@@ -125,11 +125,20 @@ class site_firewall::pre {
     provider => ['ip6tables'],
   }
 
-  firewall_multi { '999 log icmp':
-    chain    => ['INPUT', 'OUTPUT'],
-    proto    => 'ipv6-icmp',
-    jump     => 'LOG',
-    provider => ['ip6tables'],
+  firewall_multi { '999 log icmp input':
+    chain      => 'INPUT',
+    proto      => 'ipv6-icmp',
+    jump       => 'LOG',
+    log_prefix => 'INPUT ',
+    provider   => ['ip6tables'],
+  }
+
+  firewall_multi { '999 log icmp output':
+    chain      => 'OUTPUT',
+    proto      => 'ipv6-icmp',
+    jump       => 'LOG',
+    log_prefix => 'OUTPUT ',
+    provider   => ['ip6tables'],
   }
 
 }

6f3e232028ac58e446f1e679a0c6463c3870b574
gustavo panizzo <gfa@zumbi.com.ar>
2019-10-06 10:27:01 +0200

    Save a list of installed packages as a custom fact

---
+++ site/base/lib/facter/pkglist.rb
@@ -0,0 +1,12 @@
+# see site/base/manifests/facts/installed_packages.pp
+# frozen_string_literal: true
+
+pkglist = '/var/lib/misc/thishost/pkglist'
+
+if FileTest.exist?(pkglist)
+  Facter.add('pkglist') do
+    setcode do
+      open(pkglist).read.split("\n").join(' ')
+    end
+  end
+end
---
+++ site/base/manifests/facts/installed_packages.pp
@@ -0,0 +1,27 @@
+# this class provides the list of installed packages as a custom fact
+# inspired on git+ssh://$USER@puppet.debian.org/srv/puppet.debian.org/git/dsa-puppet.git
+# cead6de02810f62af1f679363843bb3444f11425
+# bugs are mine
+
+class base::facts::installed_packages {
+
+  file { '/var/lib/misc/thishost':
+    ensure => directory,
+    mode   => '0750',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  -> file { '/etc/apt/apt.conf.d/local-pkglist':
+    source => "puppet:///modules/${module_name}/local-pkglist",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  -> exec { 'dpkg list':
+    command => '/usr/bin/dpkg-query -W -f \'${Package}\n\' > /var/lib/misc/thishost/pkglist',  # lint:ignore:single_quote_string_with_variables
+    creates => '/var/lib/misc/thishost/pkglist',
+  }
+
+}

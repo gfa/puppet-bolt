7915c1d39d93eb03170c074e17d2183a6fe14915
gustavo panizzo <gfa@zumbi.com.ar>
2019-10-08 12:22:24 +0200

    take packages to pin to backports from hiera

--- site/base/manifests/packages.pp
+++ site/base/manifests/packages.pp
@@ -1,26 +1,18 @@
 # This class installs all the packages I want on _all_ machines
 #
 # @param base Contains base packages to install everywhere
+# @param backports Contains packages that should be pinned to -backports
 #
 
 class base::packages (
   Hash[String, Hash] $base,
+  Array[String] $backports,
 ) {
 
   create_resources(package, $base, {'ensure' => 'present'})
 
   apt::pin { 'backports':
-    packages => [
-      'fail2ban',
-      'ipset-persistent',
-      'iptables-persistent',
-      'netfilter-persistent',
-      'facter',
-      'prosody',
-      'prosody-modules',
-      # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=941414
-      'dehydrated',
-    ],
+    packages => $backports,
     priority => 990,
     release  => "${facts['os']['distro']['codename']}-backports",
   }

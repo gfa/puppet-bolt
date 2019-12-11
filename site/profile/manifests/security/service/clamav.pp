82fecc4d3a4fd295c2d70e846f27ea27cfcea4fb
gustavo panizzo <gfa@zumbi.com.ar>
2019-12-11 18:05:32 +0100

    manage clamav

---
+++ site/profile/manifests/security/service/clamav.pp
@@ -0,0 +1,57 @@
+# this class manages clamav and friends
+#
+# @param mirrors list with mirrors where freshclam is allowed to connect to
+#
+
+class profile::security::service::clamav (
+  Array[String] $mirrors,
+) {
+
+  apt::pin { 'clamav':
+    packages => ['clamav-daemon', 'clamav-freshclam', 'libclamav9'],
+    priority => 990,
+    release  => "${facts['os']['distro']['codename']}-backports",
+  }
+
+  -> class {'clamav':
+    manage_clamd             => true,
+    manage_freshclam         => true,
+    clamd_service_ensure     => 'running',
+    freshclam_service_ensure => 'running',
+    clamd_options            => {
+      'LogSyslog'   => true,
+      'LogFacility' => 'LOG_LOCAL6',
+      'LogClean'    => false,
+      'LogVerbose'  => false,
+    },
+    freshclam_options        => {
+      'LogSyslog'   => true,
+      'LogFacility' => 'LOG_LOCAL6',
+    }
+  }
+
+  -> profile::networking::service::dnsmasq::ipset { 'clamav-mirrors':
+    ipset_hosts => $mirrors,
+  }
+
+  -> firewall { '300 Allow network access to freshclam4':
+    chain    => 'OUTPUT',
+    dport    => ['80', '443', '873'],
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'iptables',
+    ipset    => 'clamav4 dst',
+    uid      => 'clamav',
+  }
+
+  -> firewall { '300 Allow network access to freshclam6':
+    chain    => 'OUTPUT',
+    dport    => ['80', '443', '873'],
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'ip6tables',
+    ipset    => 'clamav6 dst',
+    uid      => 'clamav',
+  }
+
+}

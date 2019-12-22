63760bdd5c09c3e324085cfe2d7929d0d8607f27
gustavo panizzo <gfa@zumbi.com.ar>
2019-12-22 23:53:49 +0100

    more improvements to the transmission profile
    [skip ci]

--- site/profile/manifests/service/transmission.pp
+++ site/profile/manifests/service/transmission.pp
@@ -1,6 +1,7 @@
 # this class manages transmission
 #
 # @param blocklist_url url where to download blocklists
+# @param blocklist_enabled enable blocklist
 # @param peer_port_random_low low port if using random ports
 # @param peer_port_random_high high port if using random ports
 # @param peer_port port to use if using static ports
@@ -19,6 +20,9 @@
 # @param incomplete_dir_enabled if enable temporary directory
 # @param home_dir home directory for transmission
 # @param download_dir directory to save downloads
+# @param ratio_limit stop seeding at this ratio
+# @param ratio_limit_enabled stop seeding after reaching a ratio
+# @param utp_enabled if utp should be enabled
 
 class profile::service::transmission (
   String $rpc_username,
@@ -30,6 +34,7 @@ class profile::service::transmission (
   Boolean $peer_port_random = false,
   Boolean $watch_dir_enabled = false,
   Stdlib::HTTPUrl $blocklist_url = 'http://www.example.com/blocklist',
+  Boolean $blocklist_enabled = false,
   Boolean $manage_ppa = false,
   Boolean $rpc_whitelist_enabled = true,
   String $rpc_whitelist = '127.0.0.1',
@@ -40,6 +45,9 @@ class profile::service::transmission (
   String $incomplete_dir = 'temp', # relative paths to $home_dir :\
   String $download_dir = 'downloads',
   Stdlib::UnixPath $home_dir = '/var/lib/transmission-daemon',
+  Boolean $ratio_limit_enabled = false,
+  Integer $ratio_limit = 2,
+  Boolean $utp_enabled = true,
 ) {
 
   include profile::networking::firewall::service::transmission
@@ -64,6 +72,9 @@ class profile::service::transmission (
     peer_port_random_on_start   => $peer_port_random,
     peer_port_random_low        => $peer_port_random_low,
     peer_port_random_high       => $peer_port_random_high,
+    ratio_limit_enabled         => $ratio_limit_enabled,
+    ratio_limit                 => $ratio_limit,
+    utp_enabled                 => $utp_enabled,
   }
 
 }

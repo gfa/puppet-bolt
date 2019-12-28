530e425273db3a6f6be3939c5121f75598f30188
gustavo panizzo <gfa@zumbi.com.ar>
2019-12-28 21:46:01 +0100

    style and documentation fixes
    [skip ci]

--- site/profile/manifests/service/nginx/default_locations.pp
+++ site/profile/manifests/service/nginx/default_locations.pp
@@ -1,15 +1,17 @@
 # this class manages the locations we configure by default
 #
+# @param vhost_name hostname of the vhost
+#
 
 define profile::service::nginx::default_locations (
   String $vhost_name,
 ) {
 
   nginx::resource::location { "${vhost_name}_dehydrated":
-     location_alias => '/var/lib/dehydrated/acme-challenges',
-     location       => '/.well-known/acme-challenge',
-     server         => $vhost_name,
-     index_files    => [],
+    location_alias => '/var/lib/dehydrated/acme-challenges',
+    location       => '/.well-known/acme-challenge',
+    server         => $vhost_name,
+    index_files    => [],
   }
 
   # not yet supported :(

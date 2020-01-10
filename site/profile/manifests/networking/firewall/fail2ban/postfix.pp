e536e598cc503bffc5a4c4c52d83f42f4ce79f77
gustavo panizzo <gfa@zumbi.com.ar>
2020-01-10 08:22:11 +0100

    bugfix on fail2ban configuration
    [skip ci]

--- site/profile/manifests/networking/firewall/fail2ban/postfix.pp
+++ site/profile/manifests/networking/firewall/fail2ban/postfix.pp
@@ -5,6 +5,15 @@ class profile::networking::firewall::fail2ban::postfix {
 
   include profile::networking::firewall::fail2ban
 
+  fail2ban::filter { 'postfix-sasl':
+    failregexes   => [
+      '^postfix(-\w+)?/(?:submission/|smtps/)?smtp[ds] warning: [-._\w]+\[<HOST>\]: SASL ((?i)LOGIN|PLAIN|(?:CRAM|DIGEST)-MD5) authentication failed(:[ A-Za-z0-9+/:]*={0,2})?\s*$',
+    ],
+    ignoreregexes => [
+      'authentication failed: Connection lost to authentication server$',
+    ],
+  }
+
   $postfix_sasl_params = lookup('fail2ban::jail::postfix-sasl')
   fail2ban::jail { 'postfix-sasl':
     action => 'iptables-multiport[name=postfix-sasl, port="smtp,submission,imap,imaps,sieve", protocol=tcp]
@@ -18,7 +27,7 @@ class profile::networking::firewall::fail2ban::postfix {
     ],
   }
 
-  -> fail2ban::jail { 'postfix-auth':
+  fail2ban::jail { 'postfix-auth':
     port    => 'smtp,submission,imap,imaps,sieve',
     filter  => 'postfix-auth',
     logpath => '%(postfix_log)s',

bbe065c270793a9131c7c4310a3c41dc32f0e328
gustavo panizzo <gfa@zumbi.com.ar>
2020-01-31 12:40:04 +0100

    finish the clamav milter class
    [skip ci]

--- site/profile/manifests/security/service/clamav/milter.pp
+++ site/profile/manifests/security/service/clamav/milter.pp
@@ -4,25 +4,81 @@
 # @param allow_connections_from an array of ip4 addr that are allowed to connect
 # @param port port where the milter will listen
 # @param listen_on ip4 address on which the milter will listen for connections
+# @param whitelist_addresses To: and From: email addresses to whitelist
+#
+# TODO: there is a lot of hardcoded stuff in here
 
 class profile::security::service::clamav::milter (
-  IP::Address::V4 $allow_connections_from_4 = '127.0.0.1',
+  Array[IP::Address::V4] $allow_connections_from = ['127.0.0.1'],
   IP::Address::V4 $listen_on = '127.0.0.1',
   Integer $port = 3310,
-  Hash $whitelist_addresses = {},
+  String $whitelist_addresses = '',
 ) {
 
   package { 'clamav-milter':
     ensure => present,
   }
 
-  -> firewall { '300 Allow network access to clamav-milter':
+  $config_options = {
+    'AddHeader'                 => 'Replace',
+    'ClamdSocket'               => 'unix:/var/run/clamav/clamd.ctl',
+    'FixStaleSocket'            => true,
+    'Foreground'                => false,
+    'LogClean'                  => 'Off',
+    'LogFacility'               => 'LOG_LOCAL6',
+    'LogFileMaxSize'            => '1M',
+    'LogFileUnlock'             => false,
+    'LogInfected'               => 'Off',
+    'LogRotate'                 => true,
+    'LogSyslog'                 => true,
+    'LogTime'                   => true,
+    'LogVerbose'                => false,
+    'MaxFileSize'               => '25M',
+    'MilterSocket'              => "inet:${port}@${$listen_on}",
+    'MilterSocketGroup'         => 'clamav',
+    'MilterSocketMode'          => '666',
+    'OnClean'                   => 'Accept',
+    'OnFail'                    => 'Defer',
+    'OnInfected'                => 'Reject',
+    'PidFile'                   => '/var/run/clamav/clamav-milter.pid',
+    'ReadTimeout'               => 120,
+    'SupportMultipleRecipients' => true,
+    'TemporaryDirectory'        => '/tmp',
+    'User'                      => 'clamav',
+    'Whitelist'                 => '/etc/clamav/whitelist',
+  }
+
+  service { 'clamav-milter':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => [Package['clamav-milter'], File['/etc/clamav/clamav-milter.conf']],
+  }
+
+  file { '/etc/clamav/clamav-milter.conf':
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('clamav/clamav.conf.erb'),
+    notify  => Service['clamav-milter'],
+  }
+
+  file { '/etc/clamav/whitelist':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    content => $whitelist_addresses,
+    notify  => Service['clamav-milter'],
+  }
+
+  firewall_multi { '300 Allow network access to clamav-milter':
     chain    => 'INPUT',
     dport    => $port,
     proto    => 'tcp',
     action   => 'accept',
     provider => 'iptables',
-    source   => $allow_connections_from_4,
+    source   => $allow_connections_from,
   }
 
 }

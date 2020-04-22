cd280d3a5ac2195c90475e7b646d01c312e4f9a0
gustavo panizzo <gfa@zumbi.com.ar>
2020-04-22 12:52:08 +0200

    Revert "configure the http_upload component in prosody"
    This reverts commit 4c0f9b8d223f88b3b144f4c5dbe56fc8a33bdf6a.

--- site/profile/manifests/service/prosody.pp
+++ site/profile/manifests/service/prosody.pp
@@ -68,15 +68,12 @@ class profile::service::prosody (
       ssl_cert       => "/etc/prosody/certs/${prosody_fqdn}/fullchain.pem",
       ssl_copy       => false,
       components     => {
-        $prosody_fqdn       => {
+        $prosody_fqdn => {
           'type'    => 'proxy65',
           'options' => {
             'proxy65_address' => "'${prosody_fqdn}'",
             'proxy65_acl'     => "'${prosody_domain}'",
           }
-        },
-        "${prosody_domain}" => {
-          'type' => 'http_upload'
         }
       },
       custom_options => {

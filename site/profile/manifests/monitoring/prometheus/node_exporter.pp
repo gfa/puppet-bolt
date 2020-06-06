1d8973b3dbc8f6c3a67bb367cae57ae0a30b3477
gustavo panizzo <gfa@zumbi.com.ar>
2020-06-06 23:53:25 +0200

    support ipv6 on prometheus node export

--- site/profile/manifests/monitoring/prometheus/node_exporter.pp
+++ site/profile/manifests/monitoring/prometheus/node_exporter.pp
@@ -1,9 +1,11 @@
 # this class manages the node exporter
 
-# @param prometheus_host host were prometheus runs
+# @param prometheus_host4 host were prometheus runs ipv4
+# @param prometheus_host6 host were prometheus runs ipv6
 
 class profile::monitoring::prometheus::node_exporter (
-  Stdlib::Host $prometheus_host = '127.0.0.1'
+  Stdlib::Host $prometheus_host4 = '127.0.0.1',
+  Stdlib::Host $prometheus_host6 = '::1',
 ) {
 
   package { 'prometheus-node-exporter':
@@ -11,11 +13,21 @@ class profile::monitoring::prometheus::node_exporter (
   }
 
   firewall { '300 allow prometheus to scrap the node exporter':
-    chain  => 'INPUT',
-    dport  => 9100,
-    proto  => 'tcp',
-    action => 'accept',
-    source => $prometheus_host,
+    chain    => 'INPUT',
+    dport    => 9100,
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'iptables',
+    source   => $prometheus_host4,
+  }
+
+  firewall { '300 allow prometheus to scrap the node exporter':
+    chain    => 'INPUT',
+    dport    => 9100,
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'ip6tables',
+    source   => $prometheus_host6,
   }
 
 }

ffe1bbaee09ed58e77b202219e0d5ef2145f16a8
gustavo panizzo <gfa@zumbi.com.ar>
2020-08-23 23:59:48 +0200

    report spam/ham in spamassassin

--- site/profile/files/service/dovecot/sa-learn-ham.sh
+++ site/profile/files/service/dovecot/sa-learn-ham.sh
@@ -5,7 +5,6 @@ TEMP=$(mktemp -p /srv/vmail/antispam/${1}/ham)
 cat - > $TEMP
 
 /usr/bin/maddr -a -h 'from:sender:reply-to' $TEMP >> $HOME/ham.txt 2>>/tmp/maddr.log
-exit 0
 
 MAX_SIZE=$(grep max-size /etc/spamassassin/spamc.conf | awk '{print $2}')
 MAIL_SIZE=$(stat --printf=%s $TEMP)

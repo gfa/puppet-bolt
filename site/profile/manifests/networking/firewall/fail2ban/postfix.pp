b727f8846a19a126ba5efdd98904a7a126a8fa23
gustavo panizzo <gfa@zumbi.com.ar>
2020-10-16 01:43:13 +0200

    fix postfix fail2ban configuration
    although it is a bit inefficient (lack of ^ in the regex)

--- site/profile/manifests/networking/firewall/fail2ban/postfix.pp
+++ site/profile/manifests/networking/firewall/fail2ban/postfix.pp
@@ -6,33 +6,16 @@ class profile::networking::firewall::fail2ban::postfix {
   include profile::networking::firewall::fail2ban
 
   fail2ban::filter { 'postfix-sasl':
-    failregexes   => [
-      '^postfix(-\w+)?/(?:submission/|smtps/)?smtp[ds] warning: [-._\w]+\[<HOST>\]: SASL ((?i)LOGIN|PLAIN|(?:CRAM|DIGEST)-MD5) authentication failed(:[ A-Za-z0-9+/:]*={0,2})?\s*$',  # lint:ignore:140chars
-    ],
-    ignoreregexes => [
-      'authentication failed: Connection lost to authentication server$',
-    ],
-  }
-
-  $postfix_sasl_params = lookup('fail2ban::jail::postfix-sasl')
-  fail2ban::jail { 'postfix-sasl':
-    action => 'iptables-multiport[name=postfix-sasl, port="smtp,submission,imap,imaps,sieve", protocol=tcp]
-         blocklist_de[service=postfix]',
-    *      => $postfix_sasl_params,
-  }
-
-  fail2ban::filter { 'postfix-auth':
     failregexes => [
-      'lost connection after AUTH from (.*)\[<HOST>\]',
+      'postfix(-\w+)?/(?:submission/|smtps/)?smtp[ds]\[[0-9]+\]: warning: [-._\w]+\[<HOST>\]: SASL ((?i)LOGIN|PLAIN|(?:CRAM|DIGEST)-MD5) authentication failed(:[ A-Za-z0-9+\/:]*={0,2})?\s*$'  # lint:ignore:140chars
     ],
   }
 
-  fail2ban::jail { 'postfix-auth':
-    port    => 'smtp,submission,imap,imaps,sieve',
-    filter  => 'postfix-auth',
-    logpath => '%(postfix_log)s',
-    backend => '%(postfix_backend)s',
-
+  $postfix_params = lookup('fail2ban::jail::postfix-sasl')
+  fail2ban::jail { 'postfix-sasl':
+    filter => 'postfix-sasl',
+    *      => $postfix_params,
   }
+
 
 }

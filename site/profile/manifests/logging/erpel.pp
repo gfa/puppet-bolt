b7ef2702bd1a02ca591b922cb414a00b340e8e1d
gustavo panizzo <gfa@zumbi.com.ar>
2020-11-22 12:14:49 +0100

    create an user and group for erpel

---
+++ site/profile/manifests/logging/erpel.pp
@@ -0,0 +1,19 @@
+# this class prepares a machine to run erpel
+# for now it only creates a user, since all other bits
+# are deployed from a pipeline
+# vim: set ts=2 sw=2 et :
+
+class loging::erpel {
+
+  group { 'erpel':
+    system         => true,
+  }
+
+  user { 'erpel':
+    system         => true,
+    forcelocal     => true,
+    purge_ssh_keys => true,
+    gid            => 'erpel',
+  }
+
+}

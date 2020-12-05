662587386438216e94a04944e4b30dc87ac45ec1
gustavo panizzo <gfa@zumbi.com.ar>
2020-12-05 13:33:40 +0100

    newer versions of fail2ban broke my custom postix-sasl filter
    [skip ci]

--- site/profile/manifests/networking/firewall/fail2ban/postfix.pp
+++ site/profile/manifests/networking/firewall/fail2ban/postfix.pp
@@ -5,16 +5,16 @@ class profile::networking::firewall::fail2ban::postfix {
 
   include profile::networking::firewall::fail2ban
 
-  fail2ban::filter { 'postfix-sasl':
+  fail2ban::filter { 'postfix-sasl-custom':
     failregexes => [
       'postfix(-\w+)?/(?:submission/|smtps/)?smtp[ds]\[[0-9]+\]: warning: [-._\w]+\[<HOST>\]: SASL ((?i)LOGIN|PLAIN|(?:CRAM|DIGEST)-MD5) authentication failed(:[ A-Za-z0-9+\/:]*={0,2})?\s*$'  # lint:ignore:140chars
     ],
   }
 
-  $postfix_sasl_params = lookup('fail2ban::jail::postfix-sasl')
-  fail2ban::jail { 'postfix-sasl':
-    filter => 'postfix-sasl',
-    *      => $postfix_sasl_params,
+  fail2ban::jail { 'postfix-sasl-custom':
+    filter  => 'postfix-sasl-custom',
+    logpath => '%(postfix_log)s',
+    backend => '%(postfix_backend)s',
   }
 
   fail2ban::filter { 'postfix-brute':

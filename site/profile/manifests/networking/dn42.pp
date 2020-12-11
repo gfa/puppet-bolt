c3f6baeb5144dd57e76da8ce0ada355086edc0f7
gustavo panizzo <gfa@zumbi.com.ar>
2020-12-11 22:36:58 +0100

    manage servers in the dn42 network

--- site/profile/manifests/networking/dn42.pp
+++ site/profile/manifests/networking/dn42.pp
@@ -3,6 +3,9 @@
 
 class profile::networking::dn42 {
 
+  include site_bird
+  include profile::networking::dn42::firewall
+
   sysctl { 'net.ipv4.ip_forward':
     ensure => present,
     value  => '1',
@@ -27,6 +30,16 @@ class profile::networking::dn42 {
   sysctl { 'net.ipv4.conf.all.rp_filter':
     ensure => present,
     value  => '0',
+  }
+
+  sysctl { 'net.ipv4.conf.all.log_martians':
+    ensure => present,
+    value  => '1',
+  }
+
+  sysctl {'net.ipv4.conf.default.log_martians':
+    ensure => present,
+    value => '1',
   }
 
 }
---
+++ site/profile/manifests/networking/dn42/firewall.pp
@@ -0,0 +1,37 @@
+# handles the firewall for dn42 hosts
+# for now only wireguard interfaces are managed
+
+
+class profile::networking::dn42::firewall {
+
+  firewall_multi { '001 do not track wg traffic in':
+    provider => ['iptables', 'ip6tables'],
+    table    => 'raw',
+    chain    => 'PREROUTING',
+    jump     => 'NOTRACK',
+    iniface => 'wg+',
+  }
+
+  firewall_multi { '001 do not track wg traffic out':
+    provider => ['iptables', 'ip6tables'],
+    table    => 'raw',
+    chain    => 'OUTPUT',
+    jump     => 'NOTRACK',
+    outiface => 'wg+',
+  }
+
+  firewall_multi { '100 allow incoming traffic on wg+':
+    provider => ['iptables', 'ip6tables'],
+    chain    => 'INPUT',
+    iniface  => 'wg+',
+    action   => 'accept',
+  }
+
+  firewall_multi { '100 allow outgoing traffic on wg+':
+    provider => ['iptables', 'ip6tables'],
+    chain    => 'OUTPUT',
+    outiface  => 'wg+',
+    action   => 'accept',
+  }
+
+}

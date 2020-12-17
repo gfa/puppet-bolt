2d5338f589953b8af8092a4f56a76bb5bff40d4c
gustavo panizzo <gfa@zumbi.com.ar>
2020-12-17 21:29:02 +0100

    configure the vnstat munin plugin for all interfaces
    [skip ci]

--- site/profile/manifests/monitoring/munin/node/plugin/vnstat.pp
+++ site/profile/manifests/monitoring/munin/node/plugin/vnstat.pp
@@ -1,12 +1,17 @@
-# this class configures the vnstat plugin for munin
+# this class configures the vnstat plugin for munin for all interfaces but lo
 #
 
 class profile::monitoring::munin::node::plugin::vnstat {
 
-  munin::plugin { "vnstat_${facts['networking']['primary']}":
-    ensure  => present,
-    source  => "puppet:///modules/${module_name}/monitoring/munin/node/plugins/vnstat_",
-    require => Package['vnstat'],
-  }
+  $interfaces = $facts['networking']['interfaces'].keys()
 
+  ($interfaces.filter | $interface | { $interface != 'lo' }).each | $iface | {
+
+    munin::plugin { "vnstat_${iface}":
+      ensure  => present,
+      source  => "puppet:///modules/${module_name}/monitoring/munin/node/plugins/vnstat_",
+      require => Package['vnstat'],
+    }
+
+  }
 }

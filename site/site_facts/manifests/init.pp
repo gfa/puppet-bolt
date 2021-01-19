9c7c504873c05f87f4e474d87bb7d3fd2e1886d4
gustavo panizzo <gfa@zumbi.com.ar>
2021-01-19 17:11:35 +0100

    save facts in a file
    long term goal is to gather all facts across the servers
    as hiera data
    
    [skip ci]

--- site/site_facts/manifests/init.pp
+++ site/site_facts/manifests/init.pp
@@ -5,4 +5,12 @@ class site_facts {
 
   include site_facts::installed_packages
 
+  file { '/var/tmp/puppet-facts':
+    ensure    => file,
+    mode      => '0600',
+    owner     => 'root',
+    group     => 'root',
+    show_diff => false,
+    content   => epp("${module_name}/var/tmp/puppet-facts.epp"),
+  }
 }

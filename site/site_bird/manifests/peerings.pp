b0beb755d9cb747fc062db12788440c699dfccb2
gustavo panizzo <gfa@zumbi.com.ar>
2021-01-22 23:47:33 +0100

    support ipv4 and ipv6 options in peerings for bird
    [skip ci]

--- site/site_bird/manifests/peerings.pp
+++ site/site_bird/manifests/peerings.pp
@@ -19,7 +19,6 @@ class site_bird::peerings (
 
       file { "/etc/bird/peers/${peer_name}.conf":
         ensure  => file,
-        replace => no,
         content => epp(
           "${module_name}/etc/bird/peers/peer.conf.epp",
           { 'peer_name' => $peer_name,
@@ -34,17 +33,7 @@ class site_bird::peerings (
       }
       # keep a copy of how the config should look like
       file { "/etc/bird/peers/${peer_name}.puppet":
-        ensure  => file,
-        content => epp(
-          "${module_name}/etc/bird/peers/peer.conf.epp",
-          { 'peer_name' => $peer_name,
-            'interface' => "wg${index}",
-          } + $peer,
-        ),
-        require => [
-          Package['wireguard'],
-          Package['bird2'],
-        ],
+        ensure  => absent,
       }
     }
 

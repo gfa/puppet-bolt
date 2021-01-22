68734b601cd60bd3b11533b2fef75c5e2e449e12
gustavo panizzo <gfa@zumbi.com.ar>
2021-01-22 22:17:34 +0100

    rename external_facts to facts_db
    copy the facts_db back to the control node and distribute it to all
    nodes
    [skip ci]

--- site/site_facts/manifests/init.pp
+++ site/site_facts/manifests/init.pp
@@ -5,13 +5,13 @@ class site_facts {
 
   include site_facts::installed_packages
 
-  file { '/var/tmp/puppet-facts':
+  file { '/var/tmp/facts_db.yaml':
     ensure    => file,
     mode      => '0600',
     owner     => 'root',
     group     => 'root',
     show_diff => false,
-    content   => epp("${module_name}/var/tmp/puppet-facts.epp"),
+    content   => epp("${module_name}/var/tmp/facts_db.yaml.epp"),
   }
 
 }

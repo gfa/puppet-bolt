4acfe69811682a81b8949835839d4eed63ac6123
gustavo panizzo <gfa@zumbi.com.ar>
2021-04-06 09:11:18 +0200

    retrieve the pgp key directly from elastic
    [skip ci]

--- site/profile/manifests/logging/journalbeat.pp
+++ site/profile/manifests/logging/journalbeat.pp
@@ -12,7 +12,7 @@ class profile::logging::journalbeat {
       # require  => Package['apt-transport-https'],
       key      => {
         'id'   => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-        server => 'pgp.mit.edu',
+        source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
       },
     }
 

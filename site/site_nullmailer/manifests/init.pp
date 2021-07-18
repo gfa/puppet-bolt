d7e3f54d27dd05f6bd30225a8cc25fa081077c2a
gustavo panizzo <gfa@zumbi.com.ar>
2021-07-19 00:17:19 +0200

    add a wrapper to sendmail to fix the from and to fields
    [skip ci]

--- site/site_nullmailer/manifests/init.pp
+++ site/site_nullmailer/manifests/init.pp
@@ -9,4 +9,22 @@ class site_nullmailer {
     require => Exec['apt_update'],
   }
 
+  # install the wrapper to make smtp auth happy (forces the "From:" field)
+  # see https://github.com/bruceg/nullmailer/issues/72
+
+  exec { 'divert sendmail':
+    command => 'dpkg-divert --divert /usr/sbin/sendmail-bin --rename /usr/sbin/sendmail',
+    creates => '/usr/sbin/sendmail-bin',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  file { '/usr/sbin/sendmail':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Exec['divert sendmail'],
+    content => epp("${module_name}/usr/sbin/sendmail.epp"),
+  }
+
 }

c0b7b11d265c9b57b177444c14c06939a20474bc
gustavo panizzo <gfa@zumbi.com.ar>
2021-07-19 21:47:34 +0200

    remove the diversion for sendmail
    since i'm going back to a proper relay
    
    [skip ci]

--- site/site_nullmailer/manifests/init.pp
+++ site/site_nullmailer/manifests/init.pp
@@ -12,19 +12,10 @@ class site_nullmailer {
   # install the wrapper to make smtp auth happy (forces the "From:" field)
   # see https://github.com/bruceg/nullmailer/issues/72
 
-  exec { 'divert sendmail':
-    command => 'dpkg-divert --divert /usr/sbin/sendmail-bin --rename /usr/sbin/sendmail',
-    creates => '/usr/sbin/sendmail-bin',
+  exec { 'un divert sendmail':
+    command => 'rm -f /usr/sbin/sendmail ; dpkg-divert --rename --remove /usr/sbin/sendmail',
+    onlyif  => 'test -f /usr/sbin/sendmail-bin',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  }
-
-  file { '/usr/sbin/sendmail':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Exec['divert sendmail'],
-    content => epp("${module_name}/usr/sbin/sendmail.epp"),
   }
 
 }

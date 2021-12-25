940f971ff463f7f8d0198e48a3b98e4af2ac1e85
gustavo panizzo <gfa@zumbi.com.ar>
2021-12-25 19:08:21 +0100

    install the node-exporter
    [skip ci]

--- site/profile/manifests/monitoring/prometheus/node_exporter.pp
+++ site/profile/manifests/monitoring/prometheus/node_exporter.pp
@@ -2,14 +2,32 @@
 
 # @param prometheus_host4 host were prometheus runs ipv4
 # @param prometheus_host6 host were prometheus runs ipv6
-
+#
 class profile::monitoring::prometheus::node_exporter (
   Stdlib::Host $prometheus_host4 = '127.0.0.1',
   Stdlib::Host $prometheus_host6 = '::1',
 ) {
 
   package { 'prometheus-node-exporter':
-    ensure => absent,
+    ensure => present,
+  }
+
+  firewall { '400 incoming prometheus ipv4':
+    chain    => 'INPUT',
+    dport    => 9100,
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'iptables',
+    source   => $prometheus_host4,
+  }
+
+  firewall { '400 incoming prometheus ipv6':
+    chain    => 'INPUT',
+    dport    => 9100,
+    proto    => 'tcp',
+    action   => 'accept',
+    provider => 'iptables',
+    source   => $prometheus_host6,
   }
 
 }

fccc5625a11bbf1ae6e212b63e2c781df7f30f13
gustavo panizzo <gfa@zumbi.com.ar>
2022-06-01 22:39:48 +0200

    take an array of addresses as prometheus hosts
    [skip ci]

--- site/profile/manifests/monitoring/prometheus/node_exporter.pp
+++ site/profile/manifests/monitoring/prometheus/node_exporter.pp
@@ -1,33 +1,33 @@
 # this class manages the node exporter
 
-# @param prometheus_host4 host were prometheus runs ipv4
-# @param prometheus_host6 host were prometheus runs ipv6
+# @param prometheus_hosts4 hosts were prometheus runs ipv4
+# @param prometheus_hosts6 hosts were prometheus runs ipv6
 #
 class profile::monitoring::prometheus::node_exporter (
-  Stdlib::Host $prometheus_host4 = '127.0.0.1',
-  Stdlib::Host $prometheus_host6 = '::1',
+  Array[Stdlib::Host] $prometheus_hosts4 = ['127.0.0.1'],
+  Array[Stdlib::Host] $prometheus_hosts6 = ['::1'],
 ) {
 
   package { 'prometheus-node-exporter':
     ensure => present,
   }
 
-  firewall { '400 incoming prometheus ipv4':
+  firewall_multi { '400 incoming prometheus ipv4':
     chain    => 'INPUT',
     dport    => 9100,
     proto    => 'tcp',
     action   => 'accept',
     provider => 'iptables',
-    source   => $prometheus_host4,
+    source   => $prometheus_hosts4,
   }
 
-  firewall { '400 incoming prometheus ipv6':
+  firewall_multi { '400 incoming prometheus ipv6':
     chain    => 'INPUT',
     dport    => 9100,
     proto    => 'tcp',
     action   => 'accept',
     provider => 'ip6tables',
-    source   => $prometheus_host6,
+    source   => $prometheus_hosts6,
   }
 
 }

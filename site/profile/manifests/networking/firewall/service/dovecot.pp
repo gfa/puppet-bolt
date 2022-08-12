b6a555d2bd769004ae0b5228dac8135ab8bc347d
gustavo panizzo <gfa@zumbi.com.ar>
2022-08-12 10:55:48 +0200

    only allow imaps connections
    [skip ci]

--- site/profile/manifests/networking/firewall/service/dovecot.pp
+++ site/profile/manifests/networking/firewall/service/dovecot.pp
@@ -1,42 +1,14 @@
 # this class manages the fw for dovecot
 #
-
+#
 class profile::networking::firewall::service::dovecot {
 
-  firewall { '300 accept hosts dovecot clients4':
+  firewall { '300 accept imaps':
     chain    => 'INPUT',
-    dport    => [143, 993, 4190],
+    dport    => 993,
     proto    => 'tcp',
     action   => 'accept',
-    provider => 'iptables',
-    ipset    => 'myhosts4 src',
-  }
-
-  firewall { '300 accept hosts dovecot clients6':
-    chain    => 'INPUT',
-    dport    => [143, 993, 4190],
-    proto    => 'tcp',
-    action   => 'accept',
-    provider => 'ip6tables',
-    ipset    => 'myhosts6 src',
-  }
-
-  firewall { '300 accept countries dovecot clients4':
-    chain    => 'INPUT',
-    dport    => [143, 993, 4190],
-    proto    => 'tcp',
-    action   => 'accept',
-    provider => 'iptables',
-    ipset    => 'countries-allow4 src',
-  }
-
-  firewall { '300 accept countries dovecot clients6':
-    chain    => 'INPUT',
-    dport    => [143, 993, 4190],
-    proto    => 'tcp',
-    action   => 'accept',
-    provider => 'ip6tables',
-    ipset    => 'countries-allow6 src',
+    provider => ['iptables', 'ip6tables'],
   }
 
 }

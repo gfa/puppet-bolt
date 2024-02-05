4688ad5e137bb56eafee803da0535d7425dd4fa9
gustavo panizzo <gfa@zumbi.com.ar>
2024-02-05 20:00:53 +0100

    make resolvconf management optional
    [skip ci]

--- site/profile/manifests/networking/service/resolvconf.pp
+++ site/profile/manifests/networking/service/resolvconf.pp
@@ -1,16 +1,19 @@
 # this class manages resolv.conf
 #
 #
-class profile::networking::service::resolvconf {
+class profile::networking::service::resolvconf (
+  Boolean $manage_resolv_conf = true,
+) {
 
-  package { 'resolvconf':
-    ensure => purged,
+  if $manage_resolv_conf {
+    package { 'resolvconf':
+      ensure => purged,
+    }
+
+    file { '/etc/resolv.conf':
+      ensure  => file,
+      require => Package['dnsmasq'],
+      content => "nameserver 127.0.0.1\n",
+    }
   }
-
-  file { '/etc/resolv.conf':
-    ensure  => file,
-    require => Package['dnsmasq'],
-    content => "nameserver 127.0.0.1\n",
-  }
-
 }

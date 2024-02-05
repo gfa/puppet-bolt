3d96a25535188b3442da51c3e2cb2276af68b6a7
gustavo panizzo <gfa@zumbi.com.ar>
2024-02-05 20:00:39 +0100

    make dnsmasq management optional

--- site/profile/manifests/networking/service/dnsmasq.pp
+++ site/profile/manifests/networking/service/dnsmasq.pp
@@ -1,27 +1,30 @@
 # this class manages dnsmasq
 #
 # @param configs_hash contains dnsmasq's config
-
+#
 class profile::networking::service::dnsmasq (
   Hash $configs_hash,
+  Boolean $manage_dnsmasq = true,
 ) {
 
-  class { 'dnsmasq':
-    purge_config_dir => true,
-    configs_hash     => $configs_hash,
-  }
+  if $manage_dnsmasq {
+    class { 'dnsmasq':
+      purge_config_dir => true,
+      configs_hash     => $configs_hash,
+    }
 
-  file { '/etc/cron.hourly/update-ipsets':
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-    source => "puppet:///modules/${module_name}/networking/service/update-ipsets",
-    notify => Exec['update_ipsets'],
-  }
+    file { '/etc/cron.hourly/update-ipsets':
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/${module_name}/networking/service/update-ipsets",
+      notify => Exec['update_ipsets'],
+    }
 
-  exec { 'update_ipsets':
-    command     => '/etc/cron.hourly/update-ipsets',
-    refreshonly => true,
+    exec { 'update_ipsets':
+      command     => '/etc/cron.hourly/update-ipsets',
+      refreshonly => true,
+    }
   }
 
 }

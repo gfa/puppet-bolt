12b20202db8241aa63dd279b159c68b0c6e6fb07
gustavo panizzo <gfa@zumbi.com.ar>
2024-06-11 22:17:12 +0200

    make tunnels optional
    [skip ci]

--- site/site_tunnels/manifests/init.pp
+++ site/site_tunnels/manifests/init.pp
@@ -4,57 +4,61 @@
 #
 class site_tunnels (
   Array[Hash] $tunnels,
+  Boolean $enabled = true,
 ) {
 
-  group { 'tunnels': }
+  if $enabled {
 
-  user { 'tunnels':
-    managehome     => true,
-    gid            => 'tunnels',
-    purge_ssh_keys => true,
-    home           => '/etc/tunnels',
-  }
+    group { 'tunnels': }
 
-  file { ['/etc/tunnels/.ssh', '/etc/tunnels']:
-    ensure  => directory,
-    mode    => '0700',
-    owner   => 'tunnels',
-    recurse => true,
-    purge   => true,
-  }
+    user { 'tunnels':
+      managehome     => true,
+      gid            => 'tunnels',
+      purge_ssh_keys => true,
+      home           => '/etc/tunnels',
+    }
 
-  $tunnels.each | Hash $data | {
-
-    file { "/etc/tunnels/.ssh/id_${data['name']}":
-      ensure  => file,
-      mode    => '0400',
+    file { ['/etc/tunnels/.ssh', '/etc/tunnels']:
+      ensure  => directory,
+      mode    => '0700',
       owner   => 'tunnels',
-      content => Sensitive($data['private_key']),
+      recurse => true,
+      purge   => true,
     }
 
-    systemd::unit_file { "${data['name']}.service":
-      ensure  => absent,
-    }
+    $tunnels.each | Hash $data | {
 
-    systemd::unit_file { "${data['name']}-tunnel.service":
-      ensure  => present,
-      enable  => true,
-      content =>  template("${module_name}/etc/systemd/system/tunnel.service.erb"),
-    }
-
-    service { "${data['name']}-tunnel":
-      ensure => running,
-      enable => true,
-    }
-
-    if lookup('manage_iptables', Boolean, undef, true) {
-      firewall_multi { "300 allow outgoing tunnel to ${data['name']}":
-        chain       => 'OUTPUT',
-        dport       => 9022,
-        action      => 'accept',
-        destination => $data['remote'],
+      file { "/etc/tunnels/.ssh/id_${data['name']}":
+        ensure  => file,
+        mode    => '0400',
+        owner   => 'tunnels',
+        content => Sensitive($data['private_key']),
       }
-    }
 
+      systemd::unit_file { "${data['name']}.service":
+        ensure  => absent,
+      }
+
+      systemd::unit_file { "${data['name']}-tunnel.service":
+        ensure  => present,
+        enable  => true,
+        content =>  template("${module_name}/etc/systemd/system/tunnel.service.erb"),
+      }
+
+      service { "${data['name']}-tunnel":
+        ensure => running,
+        enable => true,
+      }
+
+      if lookup('manage_iptables', Boolean, undef, true) {
+        firewall_multi { "300 allow outgoing tunnel to ${data['name']}":
+          chain       => 'OUTPUT',
+          dport       => 9022,
+          action      => 'accept',
+          destination => $data['remote'],
+        }
+      }
+
+    }
   }
 }

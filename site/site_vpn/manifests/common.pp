9790c2719a780a0d3703d445b08e6b37b75bea0b
gustavo panizzo <gfa@zumbi.com.ar>
2024-11-27 19:06:56 +0100

    trust vpn traffic

--- site/site_vpn/manifests/common.pp
+++ site/site_vpn/manifests/common.pp
@@ -21,6 +21,22 @@ class site_vpn::common {
     match_mark => lookup('wireguard::firewall_mark', Integer[0, 4294967295], undef, 1),
   }
 
+  firewall_multi { '100 accept incoming vpn0':
+    proto    => 'all',
+    chain    => 'INPUT',
+    action   => 'accept',
+    iniface  => 'vpn0',
+    provider => ['iptables', 'ip6tables'],
+  }
+
+  firewall_multi { '100 accept outgoing vpn0':
+    proto    => 'all',
+    chain    => 'OUTPUT',
+    action   => 'accept',
+    provider => ['iptables', 'ip6tables'],
+    outiface => 'vpn0',
+  }
+
   if lookup('wireguard::provider', Enum['systemd', 'wgquick'], undef, 'wgquick') == 'wgquick' {
     service { 'wg-quick@vpn0':
       ensure  => 'running',
